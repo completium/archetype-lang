e5868f32a5cd7391ce0dd34a52e4ee1efa6c45a8
Pierre-Yves Strub <pierre-yves@strub.nu>
2018-12-21 09:24:56 +0100

    Add high-level API for the parsing phase

--- src/intern.ml
+++
@@ -1,7 +0,0 @@
-(* -------------------------------------------------------------------- *)
-let str_to_model buff =
-  try
-    Parser.main Lexer.token buff
-  with Parseutils.ParseError exn ->
-    Format.eprintf "%a@." Parseutils.pp_parse_error exn;
-    exit 1
