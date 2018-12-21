e4a2eea091f6efad5f50d8331c9980fc8c3bf550
Pierre-Yves Strub <pierre-yves@strub.nu>
2018-12-21 08:54:02 +0100

    move to OCaml batteries

--- src/compiler.ml
+++ src/compiler.ml
@@ -1,9 +1,12 @@
-
+(* -------------------------------------------------------------------- *)
+open Core
 open Intern
 open Print
 
-let _ =
+let main () =
   let stdinbuf = Lexing.from_channel stdin in
   let model = str_to_model stdinbuf in
   let output = model_to_str model in
   print_string output
+
+let _ = main ()
