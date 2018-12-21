51c9292461dce861e9f35d58b714b95d7fd2842a
Pierre-Yves Strub <pierre-yves@strub.nu>
2018-12-21 08:55:36 +0100

    remove is_digit (in OCaml batteries)

--- src/ident.ml
+++ src/ident.ml
@@ -1,8 +1,7 @@
+(* -------------------------------------------------------------------- *)
+open Core
 
 type ident = string
 
-let cmp_ident = String.compare
-
-let is_digit = function
-  | '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' -> true
-  | _ -> false
+(* -------------------------------------------------------------------- *)
+let cmp_ident = (String.compare : ident -> ident -> int)
