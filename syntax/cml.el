e27818a14c2c35e023afe71f1174b683d1055004
Guillaume Duhamel <guillaume.duhamel@edukera.com>
2018-12-28 17:41:16 +0100

    add logic keyword in cml mode

--- syntax/cml.el
+++ syntax/cml.el
@@ -17,10 +17,13 @@
 (setq cml-var-regexp "<%[a-z A-z][a-z A-z _]+>")
 
 
+;; Faces for Font Lock
+;;   https://www.gnu.org/software/emacs/manual/html_node/elisp/Faces-for-Font-Lock.html
 (defconst cml-font-lock-keywords-1
   (list
    `(,(cml-regexp-opt '("use" "model" "extension" "constant" "value" "role" "asset" "value" "states" "enum" "transition" "transaction" "object" "key" "namespace")) . font-lock-type-face)
-   `(,(cml-regexp-opt '("identified" "sorted" "by" "as" "from" "to" "with" "ref" "fun" "=>" "collection" "queue" "stack" "set" "subset" "partition" "asset" "assert" "object" "initial" "ensure" "args" "called" "condition" "transferred" "action" "let" "if" "then" "else" "for" "in" "break" "of" "transfer" "back" "and" "or" "not" "forall" "exists")) . font-lock-keyword-face)
+   `(,(cml-regexp-opt '("identified" "sorted" "by" "as" "from" "to" "with" "ref" "fun" "=>" "collection" "queue" "stack" "set" "subset" "partition" "asset" "object" "initial" "args" "called" "condition" "transferred" "action" "let" "if" "then" "else" "for" "in" "break" "of" "transfer" "back")) . font-lock-keyword-face)
+   `(,(cml-regexp-opt '("and" "or" "not" "->" "<->" "forall" "exists" "ensure" "assert")) . font-lock-builtin-face)
    `(,cml-ext-regexp . font-lock-preprocessor-face)
    `(,cml-var-regexp . font-lock-variable-name-face)
 
