9aaf2d3991f41e4d52fea77a21868452676c0fd6
Guillaume Duhamel <guillaume.duhamel@edukera.com>
2019-02-08 19:04:46 +0100

    fix literals

--- syntax/cml.el
+++ syntax/cml.el
@@ -9,6 +9,9 @@
         (modify-syntax-entry ?* ". 23" synTable)
         synTable))
 
+(defvar cml-mode-map nil
+  "Keymap for Cml major mode")
+
 (defun cml-regexp-opt (l)
   (regexp-opt l 'symbols))
 
@@ -16,7 +19,16 @@
 (setq cml-ext-regexp "\\[%[^]]*\\]")
 (setq cml-var-regexp "<%[a-z A-z][a-z A-z _]+>")
 (setq cml-address-regexp "@[0-9 a-z A-z _]+")
-
+(setq cml-number-regexp "[0-9]+")
+(setq cml-float-regexp "[0-9]+\\.[0-9]+")
+;;(setq cml-date-regexp "(-?(?:[1-9][0-9]*)?[0-9]{4})-(1[0-2]|0[1-9])-(3[01]|0[1-9]|[12][0-9])T(2[0-3]|[01][0-9]):([0-5][0-9]):([0-5][0-9])(.[0-9]+)?(Z)?")
+;;(setq cml-duration-regexp "[0-9]+Y")
+;;(setq cml-date-regexp
+;;(concat
+;; (format-time-string "%Y-%m-%dT%T")
+;; ((lambda (x) (concat (substring x 0 3) ":" (substring x 3 5)))
+;;  (format-time-string "%z")))
+;;)
 
 ;; Faces for Font Lock
 ;;   https://www.gnu.org/software/emacs/manual/html_node/elisp/Faces-for-Font-Lock.html
@@ -24,7 +36,11 @@
   (list
    `(,cml-ext-regexp . font-lock-preprocessor-face)
    `(,cml-var-regexp . font-lock-variable-name-face)
+;;   `(,cml-date-regexp . font-lock-builtin-face)
+;;   `(,cml-duration-regexp . font-lock-builtin-face)
    `(,cml-address-regexp . font-lock-builtin-face)
+;;   `(,cml-number-regexp . font-lock-builtin-face)
+;;   `(,cml-float-regexp . font-lock-builtin-face)
    ;; logical function : "mem" "idem" "before" "after" "fixed" "subset"
    `(,(cml-regexp-opt '("state" "now" "transferred" "caller" "fail" "balance" "conditions" "actions" "none")) . font-lock-constant-face)
    `(,(cml-regexp-opt '("use" "model" "extension" "constant" "variable" "role" "asset" "states" "enum" "transaction" "function" "object" "key" "namespace" "contract")) . font-lock-type-face)
@@ -32,14 +48,36 @@
    `(,(cml-regexp-opt '("and" "or" "not" "->" "<->" "forall" "exists" "assert" "true" "false")) . font-lock-doc-face)
 
    )
-  "Minimal highlighting for Cml mode")
+  "Minimal highlighting for cml mode")
 
 (defvar cml-font-lock-keywords cml-font-lock-keywords-1
-  "Default highlighting for Cml mode")
+  "Default highlighting for cml mode")
+
+(defvar cml-indent 2
+  "How many spaces to indent in cml mode.")
+(make-variable-buffer-local 'cml-indent)
+
+
+(defun cml-indent-line ()
+  "Indent current line as cml logic"
+  (interactive)
+  (save-excursion
+    (beginning-of-line)
+    (if (looking-at "model")  ; Check for rule 1
+        (indent-line-to 0)
+      )))
 
 (define-derived-mode cml-mode fundamental-mode "cml"
   "major mode for editing cml language code."
   (set (make-local-variable 'font-lock-defaults) '(cml-font-lock-keywords))
+  ; indentation
+  (make-local-variable 'indent-line-function)
+  (setq indent-line-function 'cml-indent-line)
+  ; providing the mode
+  (setq major-mode 'cml-mode)
+  (setq mode-name "Cml")
+  (use-local-map cml-mode-map)
+  (font-lock-mode 1)
 )
 
 (provide 'cml)
