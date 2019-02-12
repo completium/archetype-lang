05b01859e189bfd8b75c31e0a418379ff6f07f10
Guillaume Duhamel <guillaume.duhamel@edukera.com>
2019-02-12 01:08:51 +0100

    update plugin generation process

--- src/Makefile.plugin.mk
+++ src/Makefile.plugin.mk
@@ -1,60 +1,11 @@
-# -*- Makefile -*-
-
-# --------------------------------------------------------------------
 .PHONY: all clean
 
-OCAMLC=ocamlc -g -rectypes
-INCLUDES= -I /home/guillaume/.opam/cml/lib/batteries -I /home/guillaume/.opam/cml/lib/menhirLib -I /home/guillaume/.opam/cml/lib/why3
-OCAMLOPT=ocamlopt
-OCAMLDEP=ocamldep
-MENHIR=menhir
-OCAMLLEX=ocamllex
+all: plugin
 
-.SUFFIXES: .ml .mli .cmo .cmi .cmx .mll .mly
-
-# --------------------------------------------------------------------
-all: $(COMPILER) $(GENWHY3) $(GENMODELWS) .merlin
-
-.mll.ml:
-	$(OCAMLLEX) $<
-	rm -f lexer.mli
-
-.mly.ml:
-	$(MENHIR) $<
-	rm -f parser.mli
-
-.ml.cmo:
-	$(OCAMLC) $(INCLUDES) -c $<
-
-.ml.cmx:
-	$(OCAMLOPT) $(INCLUDES) -c $<
-
-SRC = \
-  core.ml \
-  ident.ml \
-  location.ml \
-  parseUtils.ml \
-  parseTree.ml \
-  parser.ml \
-  lexer.ml \
-  io.ml \
-  model.ml \
-  modelws.ml \
-  modelw3liq.ml \
-  printer.ml \
-  translate.ml \
-  miles.ml \
-  w3plugin.ml
-
-OBJS = $(SRC:.ml=.cmx)
-
-plugin: $(OBJS)
-	ocamlopt -shared -o cml.cmxs $(OBJS) /home/guillaume/.opam/cml/lib/batteries/batteries.cmxa
-
-#dune build --profile=$(PROFILE) w3plugin.cmxs
+plugin:
+	ocamlopt -w @a-4-29-40-41-42-44-45-48-58-59-60-40 -strict-sequence -strict-formats -short-paths -keep-locs -g -w Y -w Z -w -9 -w -23 -w +28 -w +33 -w -32 -g -shared -linkall -I . -o _build/default/cml.cmxs /home/dev/.opam/cml/lib/result/result.cmxa /home/dev/.opam/cml/lib/ppx_deriving/ppx_deriving_runtime.cmxa /home/dev/.opam/cml/lib/ocaml/unix.cmxa /home/dev/.opam/cml/lib/ocaml/nums.cmxa /home/dev/.opam/cml/lib/ocaml/bigarray.cmxa /home/dev/.opam/cml/lib/ocaml/str.cmxa /home/dev/.opam/cml/lib/ocaml/threads/threads.cmxa /home/dev/.opam/cml/lib/batteries/batteries.cmxa /home/dev/.opam/cml/lib/batteries/batteriesThread.cmxa _build/default/cmlLib.cmxa _build/default/w3plugin.cmxa
 
 clean:
-	dune clean
 	rm -fr *~ \#*
 	rm -fr *.cm[aoix]
 	rm -fr *.cmxs
