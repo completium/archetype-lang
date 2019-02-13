780e439a67295aa82b340536007847dd03513fef
Guillaume Duhamel <guillaume.duhamel@edukera.com>
2019-02-13 16:07:58 +0100

    update liquidity generation

--- generateLiquidity.sh
+++ generateLiquidity.sh
@@ -2,4 +2,4 @@
 
 cat why3/lib/prelude.liq > test.liq
 ./extract.sh $1 >> test.liq
-echo "let%entry main (_) s = (empty_ops, s)" >> test.liq
+echo "let%entry main (_param : unit) s = (empty_ops, s)" >> test.liq
