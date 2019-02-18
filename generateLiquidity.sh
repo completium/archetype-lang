b364487d47c2a43990cd22d3ff6091083016e05a
Guillaume Duhamel <guillaume.duhamel@edukera.com>
2019-02-18 17:52:19 +0100

    update script

--- generateLiquidity.sh
+++ generateLiquidity.sh
@@ -1,5 +1,17 @@
-#!
+#! /bin/bash
 
-cat why3/lib/prelude.liq > test.liq
-./extract.sh $1 >> test.liq
-echo "let%entry main (_param : unit) s = (empty_ops, s)" >> test.liq
+if [ $# -gt 0 ] && [ -f ${1} ]; then
+    tmpliq=$(mktemp /tmp/liq.XXXXXX)
+    cat ./why3/lib/prelude.liq > $tmpliq
+    why3 extract -L ~/cml-lang/why3/lib -F cml_lang -D liquidity $1 >> $tmpliq
+    echo "let%entry main (_param : unit) s = (empty_ops, s)" >> $tmpliq
+    str=$(sed -e ':a;N;$!ba;s/\n/\\n/g' -e 's/\"/\\\"/g' < $tmpliq)
+    tmpjs=$(mktemp /tmp/js.XXXXXX)
+    echo "var str = \"" "$str" "\"; var s = encodeURIComponent(str).replace(/\"/g,\"%27\").replace(/\"/g,\"%22\"); console.log(s);" > $tmpjs
+    param=`nodejs $tmpjs`
+    url="http://www.liquidity-lang.org/edit/?source=$param"
+    echo $url
+    python -m webbrowser $url
+else
+    echo "first argument must be a valid path to cml file."
+fi
