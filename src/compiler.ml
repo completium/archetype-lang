e6f5538be450bf4c31b61251813a48d7303531da
Guillaume Duhamel <guillaume.duhamel@edukera.com>
2019-06-01 23:38:31 +0200

    refactor toolchain in compiler module

--- src/compiler.ml
+++ src/compiler.ml
@@ -5,52 +5,67 @@ open Core
 exception Compiler_error
 exception E_arg
 exception ArgError of string
+exception Stop
+
+
+let parse (filename, channel) =
+  let pt = Io.parse_archetype ~name:filename channel in
+  if !Option.opt_json then (Format.printf "%s\n" (Yojson.Safe.to_string (ParseTree.archetype_to_yojson pt)); raise Stop)
+  else if !Option.opt_parse then (Format.printf "%a\n" ParseTree.pp_archetype pt; raise Stop)
+  else if !Option.opt_pretty_print then (() (*Format.printf "%a" Printer.pp_archetype pt*); raise Stop)
+  else pt
+
+let preprocess_ext pt =
+  if !Option.opt_pre_json then (Format.printf "%s\n" (Yojson.Safe.to_string (ParseTree.archetype_to_yojson pt)); raise Stop)
+  else if !Option.opt_pre_parse then (Format.printf "%a\n" ParseTree.pp_archetype pt; raise Stop)
+  else if !Option.opt_pre_pretty_print then (() (*Format.printf "%a" Printer.pp_archetype pt*); raise Stop)
+  else pt
+
+let type_ pt =
+  let ast = Model.mk_model (Location.dumloc "mymodel") (*Translate.parseTree_to_model pt*) in
+  if !Option.opt_ast
+  then (Format.printf "%a\n" Model.pp_model ast; raise Stop)
+  else ast
+
+let reduce_ast ast =
+  let rast = Reduce.reduce_ast ast in
+  if !Option.opt_astr
+  then (Format.printf "%a\n" Model.pp_model rast; raise Stop)
+  else rast
+
+let model ast =
+  let model = Translate.ast_to_model ast in
+  if !Option.opt_model
+  then (Format.printf "%a\n" Storage.pp_model model; raise Stop)
+  else model
+
+let generate_target model =
+  match !Option.target with
+  | Liquidity -> (
+      let liq_tree = Gen_liquidity.model_to_liq_tree model in
+      if !Option.opt_raw_target
+      then  Format.printf "%a\n" Gen_liquidity.pp_liq_tree liq_tree
+      else () (*TODO: pretty print liquidity tree *)
+    )
+  | Whyml     -> (
+      let _decls = Gen_whyml.model_to_liq_tree model in
+      if !Option.opt_raw_target
+      then () (*TODO: raw print ptree whyml tree *)
+      else () (*TODO: pretty print ptree whyml tree *)
+    )
+  | Markdown  -> () (*TODO*)
+  | None      -> () (*TODO*)
 
 (* -------------------------------------------------------------------- *)
 let compile_and_print (filename, channel) =
   Tools.debug_mode := !Option.debug_mode;
-  if !Option.opt_lsp
-  then () (*Lsp.process (filename, channel)*)
-  else (
-    let pt = Io.parse_archetype ~name:filename channel in
-    if !Option.opt_json
-    then Format.printf "%s\n" (Yojson.Safe.to_string (ParseTree.archetype_to_yojson pt))
-    else (
-      if !Option.opt_pretty_print
-      then () (*Format.printf "%a" Printer.pp_archetype pt*)
-      else (
-        if !Option.opt_parse
-        then Format.printf "%a\n" ParseTree.pp_archetype pt
-        else (
-          let ast = Model.mk_model (Location.dumloc "mymodel") (*Translate.parseTree_to_model pt*) in
-          if !Option.opt_ast
-          then Format.printf "%a\n" Model.pp_model ast
-          else (
-            let ast = Reduce.reduce_ast ast in
-            if !Option.opt_astr
-            then Format.printf "%a\n" Model.pp_model ast
-            else (
-              let model = Translate.ast_to_model ast in
-              if !Option.opt_model
-              then Format.printf "%a\n" Storage.pp_model model
-              else (
-                match !Option.target with
-                | Liquidity -> (
-                    let liq_tree = Gen_liquidity.model_to_liq_tree model in
-                    if !Option.opt_raw_target
-                    then  Format.printf "%a\n" Gen_liquidity.pp_liq_tree liq_tree
-                    else () (*TODO: pretty print liquidity tree *)
-                  )
-                | Whyml      -> (
-                    let _decls = Gen_whyml.model_to_liq_tree model in
-                    if !Option.opt_raw_target
-                    then () (*TODO: raw print ptree whyml tree *)
-                    else () (*TODO: pretty print ptree whyml tree *)
-                  )
-                | Markdown  -> () (*TODO*)
-                | None      -> () (*TODO*)
-              )
-            ))))))
+  (filename, channel)
+  |> parse
+  |> preprocess_ext
+  |> type_
+  |> reduce_ast
+  |> model
+  |> generate_target
 
 let close dispose channel =
   if dispose then close_in channel
@@ -79,8 +94,10 @@ let main () =
               "Unknown policy %s (use record, flat)@." s;
             exit 2), "<policy> Set storage policy";
       "--list-storage-policy", Arg.Unit (fun _ -> Format.printf "storage policy available:@\n  record@\n  flat@\n"; exit 0), " List storage policy";
-      "-PP", Arg.Set Option.opt_pretty_print, " Pretty print";
+      "-PP", Arg.Set Option.opt_pretty_print, " Pretty print parse tree";
       "--pretty-print", Arg.Set Option.opt_pretty_print, " Same as -PP";
+      "-PPP", Arg.Set Option.opt_pretty_print, " Pretty print parse tree without extension";
+      "--preprocess-pretty-print", Arg.Set Option.opt_pretty_print, " Same as -PPP";
       "-P", Arg.Set Option.opt_parse, " Print raw parse tree";
       "-A", Arg.Set Option.opt_ast, " Print raw ast";
       "--ast", Arg.Set Option.opt_ast, " Same as -A";
@@ -118,6 +135,8 @@ let main () =
         in*)
 
   with
+  | Stop ->
+    close dispose channel
   | ParseUtils.ParseError exn ->
     close dispose channel;
     Format.eprintf "%a@." ParseUtils.pp_parse_error exn;
