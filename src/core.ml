79815f65e53251b009d8d9bf7e21839e7ecae67b
Guillaume Duhamel <guillaume.duhamel@edukera.com>
2019-07-06 00:00:38 +0200

    update with new version of deps

--- src/core.ml
+++ src/core.ml
@@ -24,8 +24,8 @@ type big_int = Big_int.big_int
 
 let pp_big_int fmt n = Format.fprintf fmt "%s" (Big_int.string_of_big_int n)
 
-let big_int_of_yojson (s : Yojson.Safe.json) : (big_int, string) Result.result =
+let big_int_of_yojson (s : Yojson.Safe.t) : (big_int, string) Result.result =
   try Ok (Big_int.big_int_of_string (Yojson.Safe.to_string s))
   with _ -> Error "big_int_of_yojson"
 
-let big_int_to_yojson (n : big_int) : Yojson.Safe.json = Yojson.Safe.from_string (Big_int.string_of_big_int n)
+let big_int_to_yojson (n : big_int) : Yojson.Safe.t = Yojson.Safe.from_string (Big_int.string_of_big_int n)
