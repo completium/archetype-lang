f1db113615501714e2fd19d8ac68687a305820fd
Guillaume Duhamel <guillaume.duhamel@edukera.com>
2019-08-28 19:10:35 +0200

    update model 2

--- src/model.ml
+++ src/model.ml
@@ -1066,18 +1066,18 @@ type ctx_model = (lident, unit) ctx_model_gen
 let mk_ctx_model ?(formula = false) ?fs ?label ?spec_id ?invariant_id custom : ('id, 't) ctx_model_gen =
   { formula; fs; label; spec_id; invariant_id; custom}
 
-let map_mterm_model_exec (f : ctx_model -> mterm -> mterm) (model : model) : model =
-  let map_storage_item (ctx : ctx_model) (si : storage_item) : storage_item = (
+let map_mterm_model_exec (f : ('id, 't) ctx_model_gen -> mterm -> mterm) (model : model) : model =
+  let map_storage_item (ctx : ('id, 't) ctx_model_gen) (si : storage_item) : storage_item = (
     { si with
       default = f ctx si.default;
     }
   ) in
-  let map_function_struct (ctx : ctx_model) (fs : function_struct) : function_struct =
+  let map_function_struct (ctx : ('id, 't) ctx_model_gen) (fs : function_struct) : function_struct =
     let ctx = { ctx with fs = Some fs } in
     let body = f ctx fs.body in
     { fs with body = body }
   in
-  let map_function (ctx : ctx_model) (fun_ : function__) : function__ = (
+  let map_function (ctx : ('id, 't) ctx_model_gen) (fun_ : function__) : function__ = (
     let node = match fun_.node with
       | Function (fs, ret) -> Function (map_function_struct ctx fs, ret)
       | Entry fs -> Entry (map_function_struct ctx fs)
@@ -1093,39 +1093,39 @@ let map_mterm_model_exec (f : ctx_model -> mterm -> mterm) (model : model) : mod
     storage = storage;
   }
 
-let map_mterm_model_formula (f : ctx_model -> mterm -> mterm) (model : model) : model =
-  let map_verification (ctx : ctx_model) (f : ctx_model -> mterm -> mterm) (v : verification) : verification = (
-    let map_label_term (f : ctx_model -> mterm -> mterm) (lt : label_term) : label_term =
+let map_mterm_model_formula (f : ('id, 't) ctx_model_gen -> mterm -> mterm) (model : model) : model =
+  let map_verification (ctx : ('id, 't) ctx_model_gen) (f : ('id, 't) ctx_model_gen -> mterm -> mterm) (v : verification) : verification = (
+    let map_label_term (f : ('id, 't) ctx_model_gen -> mterm -> mterm) (lt : label_term) : label_term =
       let ctx = { ctx with label = lt.label } in
       { lt with
         term = f ctx lt.term }
     in
 
-    let map_predicate (f : ctx_model -> mterm -> mterm) (p : predicate) : predicate =
+    let map_predicate (f : ('id, 't) ctx_model_gen -> mterm -> mterm) (p : predicate) : predicate =
       { p with
         args = List.map (fun (x, y) -> (x, y)) p.args;
         body = f ctx p.body;
       }
     in
 
-    let map_definition (f : ctx_model -> mterm -> mterm) (d : definition) : definition =
+    let map_definition (f : ('id, 't) ctx_model_gen -> mterm -> mterm) (d : definition) : definition =
       { d with
         body = f ctx d.body
       }
     in
 
-    let map_invariantt (f : ctx_model -> mterm -> mterm) ((it_id, it_lt) : 'id * 'id label_term_gen list) : 'id * 'id label_term_gen list =
+    let map_invariantt (f : ('id, 't) ctx_model_gen -> mterm -> mterm) ((it_id, it_lt) : 'id * 'id label_term_gen list) : 'id * 'id label_term_gen list =
       (it_id, List.map (map_label_term f) it_lt)
     in
 
-    let map_invariant (f : ctx_model -> mterm -> mterm) (spec : invariant) : invariant =
+    let map_invariant (f : ('id, 't) ctx_model_gen -> mterm -> mterm) (spec : invariant) : invariant =
       let ctx = {ctx with invariant_id = Some spec.label } in
       { spec with
         formulas = List.map (f ctx) spec.formulas;
       }
     in
 
-    let map_specification (f : ctx_model -> mterm -> mterm) (spec : specification) : specification =
+    let map_specification (f : ('id, 't) ctx_model_gen -> mterm -> mterm) (spec : specification) : specification =
       let ctx = { ctx with spec_id = Some spec.name} in
       { spec with
         formula = f ctx spec.formula;
@@ -1133,7 +1133,7 @@ let map_mterm_model_formula (f : ctx_model -> mterm -> mterm) (model : model) : 
       }
     in
 
-    let map_variable (f : ctx_model -> mterm -> mterm) (spec : variable) : variable =
+    let map_variable (f : ('id, 't) ctx_model_gen -> mterm -> mterm) (spec : variable) : variable =
       spec
     in
 
@@ -1150,9 +1150,9 @@ let map_mterm_model_formula (f : ctx_model -> mterm -> mterm) (model : model) : 
     }
   ) in
 
-  let ctx : ctx_model = mk_ctx_model () in
+  let ctx : ('id, 't) ctx_model_gen = mk_ctx_model () in
 
-  let map_function (f : ctx_model -> mterm -> mterm) (fun_ : function__) : function__ =
+  let map_function (f : ('id, 't) ctx_model_gen -> mterm -> mterm) (fun_ : function__) : function__ =
     let fs : function_struct =
       match fun_.node with
       | Function (fs, _) -> fs
@@ -1170,7 +1170,7 @@ let map_mterm_model_formula (f : ctx_model -> mterm -> mterm) (model : model) : 
   }
 
 
-let map_mterm_model (f : ctx_model -> mterm -> mterm) (model : model) : model =
+let map_mterm_model (f : ('id, 't) ctx_model_gen -> mterm -> mterm) (model : model) : model =
   model
   |> map_mterm_model_exec f
   |> map_mterm_model_formula f
