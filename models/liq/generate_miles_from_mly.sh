9326927c6bf023fba6dd270a35b4c8074f7df1c6
Guillaume Duhamel <guillaume.duhamel@edukera.com>
2019-09-27 14:13:53 +0200

    remove obsolete model directory

--- models/liq/generate_miles_from_mly.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-
-OUT=miles_with_expiration_from_mlw.liq
-cat prelude.liq > $OUT
-../../src/_build/default/liq/extract.exe \
-    | sed s/by_expiration\ \(\)/by_expiration/g \
-    | sed s/get_mile_amount\ \(\)/get_mile_amount/g \
-          >> $OUT
