a80559ee7ebc4f05cf03c7b2b02217c0789306b2
Guillaume Duhamel <guillaume.duhamel@edukera.com>
2019-10-16 15:04:10 +0200

    add why3 library

--- src/options.ml
+++ src/options.ml
@@ -24,7 +24,7 @@ type sorting_policy =
   | None
 [@@deriving show {with_path = false}]
 
-let version = "0.1.6"
+let version = "0.1.7"
 let url = "https://archetype-lang.org/"
 
 let target = ref (None : target_lang)
