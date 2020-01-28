44667a1cf3e825dd40af0a4f7c927c1637467377
Guillaume Duhamel <guillaume.duhamel@edukera.com>
2020-01-28 18:32:02 +0100

    sync

--- src/printer_model_solidity.ml
+++ src/printer_model_solidity.ml
@@ -133,6 +133,7 @@ let pp_model fmt (model : model) =
           f i
       | Mremovefield _ -> pp_str fmt "todo_Mremovefield"
       | Mremoveif _    -> pp_str fmt "todo_Mremoveif"
+      | Maddupdate _      -> pp_str fmt "todo_Maddupdate"
       | Mupdate _      -> pp_str fmt "todo_Mupdate"
       | Mselect _      -> pp_str fmt "todo_Mselect"
       | Msort _        -> pp_str fmt "todo_Msort"
