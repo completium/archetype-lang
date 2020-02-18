86b784fb4a8ac473abcbd309ecbecd5a1b225298
Guillaume Duhamel <guillaume.duhamel@edukera.com>
2020-02-18 16:17:06 +0100

    update contract script

--- check_contracts.sh
+++ check_contracts.sh
@@ -46,48 +46,48 @@ voting_process.arl \
 "
 
 VERIF_CONTRACTS="\
-auction_no_memory.arl \
-c3n.arl \
-certificate_generator.arl \
-clause_io_acceptance_of_delivery.arl \
-escrow_penalty.arl \
 escrow_without_spec.arl \
-guarantee_fund.arl \
-hello.arl \
 miles_with_expiration.arl \
 miles_with_expiration_simple.arl \
-mini_dao.arl \
 mwe_medium.arl \
-register_candidate.arl \
-zero_coupon_bond.arl \
-zero_coupon_bond_with_insurance.arl \
 "
 
 REMAINED_VERIF_CONTRACTS="\
 animal_tracking.arl \
 auction.arl \
 auction_lazy.arl \
+auction_no_memory.arl \
 auction_zilliqa.arl \
 autocallable.arl \
 bond.arl \
+c3n.arl \
 c3n_without_loop.arl \
 certification_token.arl \
+certificate_generator.arl \
+clause_io_acceptance_of_delivery.arl \
 coase.arl \
 competition.arl \
 empty.arl \
+escrow_penalty.arl \
 erc20.arl \
 erc721.arl \
 escrow_basic.arl \
 escrow_simple.arl \
 fizzy.arl \
+guarantee_fund.arl \
 health_care.arl \
+hello.arl \
 ico.arl \
 miles.arl \
+mini_dao.arl \
 perishable.arl \
 register_vote.arl \
+register_candidate.arl \
 sig_challenge.arl \
 vehicle_lifecycle.arl \
 voting_process.arl \
+zero_coupon_bond.arl \
+zero_coupon_bond_with_insurance.arl \
 "
 
 RET=0
