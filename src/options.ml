5b39f09e67ee9fbf3ec872ec09bdf41fa223b29c
Guillaume Duhamel <guillaume.duhamel@edukera.com>
2020-03-05 12:48:34 +0100

    next version

--- src/options.ml
+++ src/options.ml
@@ -25,7 +25,7 @@ type sorting_policy =
   | None
 [@@deriving show {with_path = false}]
 
-let version = "0.1.13"
+let version = "0.1.14"
 let url = "https://archetype-lang.org/"
 
 let target = ref (None : target_lang)
