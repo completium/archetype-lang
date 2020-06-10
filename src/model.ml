309ce2dc629080ccdcafcc03870cd62f39027759
Benoît Rognier <benoit.rognier@edulera.com>
2020-06-10 09:49:36 +0200

    fix

--- src/model.ml
+++ src/model.ml
@@ -3166,7 +3166,7 @@ end = struct
   let type_to_asset = function
     | Tasset n -> n |> unloc
     | Tcontainer (Tasset n, _) -> n |> unloc
-    | _ as t -> Format.printf "%a" pp_type_ t; emit_error NotanAssetType
+    | _ -> emit_error NotanAssetType
 
   let get_asset_type (t : mterm) : ident = type_to_asset t.type_
 
