20353d22d6ef07284dd2a4e6074280305d8db286
Guillaume Duhamel <guillaume.duhamel@edukera.com>
2020-06-11 12:25:55 +0200

    fix check script

--- check_contracts.sh
+++ check_contracts.sh
@@ -17,14 +17,11 @@ erc20.arl \
 erc721.arl \
 escrow_penalty.arl \
 escrow_without_spec.arl \
-fizzy.arl \
 guarantee_fund.arl \
 health_care.arl \
 hello.arl \
 miles.arl \
-miles_with_expiration_simple.arl \
 mini_dao.arl \
-mwe_medium.arl \
 perishable.arl \
 register_candidate.arl \
 register_vote.arl \
@@ -38,8 +35,11 @@ animal_tracking.arl \
 autocallable.arl \
 escrow_basic.arl \
 escrow_simple.arl \
+fizzy.arl \
 ico.arl \
+miles_with_expiration_simple.arl \
 miles_with_expiration.arl \
+mwe_medium.arl \
 voting_process.arl \
 "
 
@@ -60,14 +60,11 @@ erc20.arl \
 erc721.arl \
 escrow_penalty.arl \
 escrow_without_spec.arl \
-fizzy.arl \
 guarantee_fund.arl \
 health_care.arl \
 hello.arl \
-miles_with_expiration_simple.arl \
 miles.arl \
 mini_dao.arl \
-mwe_medium.arl \
 perishable.arl \
 register_candidate.arl \
 register_vote.arl \
@@ -81,21 +78,28 @@ animal_tracking.arl \
 autocallable.arl \
 escrow_basic.arl \
 escrow_simple.arl \
+fizzy.arl \
 ico.arl \
+miles_with_expiration_simple.arl \
 miles_with_expiration.arl \
+mwe_medium.arl \
 voting_process.arl \
 "
 
 VERIF_CONTRACTS="\
 auction_lazy.arl \
 auction_no_memory.arl \
+auction_zilliqa.arl \
 auction.arl \
 bond.arl \
 c3n.arl \
 certificate_generator.arl \
+certification_token.arl \
 clause_io_acceptance_of_delivery.arl \
+coase.arl \
 competition.arl \
 empty.arl \
+erc20.arl \
 erc721.arl \
 escrow_penalty.arl \
 escrow_without_spec.arl \
@@ -106,6 +110,7 @@ miles.arl \
 mini_dao.arl \
 perishable.arl \
 register_candidate.arl \
+register_vote.arl \
 sig_challenge.arl \
 zero_coupon_bond_with_insurance.arl \
 zero_coupon_bond.arl \
@@ -113,11 +118,7 @@ zero_coupon_bond.arl \
 
 REMAINED_VERIF_CONTRACTS="\
 animal_tracking.arl \
-auction_zilliqa.arl \
 autocallable.arl \
-certification_token.arl \
-coase.arl \
-erc20.arl \
 escrow_basic.arl \
 escrow_simple.arl \
 fizzy.arl \
@@ -125,7 +126,6 @@ ico.arl \
 miles_with_expiration_simple.arl \
 miles_with_expiration.arl \
 mwe_medium.arl \
-register_vote.arl \
 voting_process.arl \
 "
 
