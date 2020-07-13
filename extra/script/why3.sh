5951db370e074b8e111ca2cf6d3df7457028b812
Benoît Rognier <benoit.rognier@edulera.com>
2020-07-14 00:04:09 +0200

    fix why3.sh

--- extra/script/why3.sh
+++ extra/script/why3.sh
@@ -3,5 +3,5 @@
 name="$(basename -- $1)"
 id=${name%.*}
 
-archetype -t whyml $1 > $id.mlw
+./archetype.exe --set-caller-init=tz1Lc2qBKEWCBeDU8npG6zCeCqpmaegRi6Jg -t whyml $1 > $id.mlw
 why3 -L ./mlw/ prove $id.mlw
