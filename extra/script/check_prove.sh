499677da3f85756dda769d62c6515c72d6d87ef9
Guillaume Duhamel <guillaume.duhamel@edukera.com>
2020-07-14 04:02:53 +0200

    fix script

--- extra/script/check_prove.sh
+++ extra/script/check_prove.sh
@@ -7,7 +7,7 @@ TOTAL=0
 clean() {
     name="$(basename -- $1)"
     id=${name%.*}
-    rm $id.mlw
+    rm -f $id.mlw
 }
 
 process_files() {
@@ -17,10 +17,10 @@ process_files() {
             printf '%-60s' $i
             ./extra/script/prove.sh $i > /dev/null 2> /dev/null
             if [ $? -ne 0 ]; then
-                echo -ne "\033[31m KO \033[0m"
+                echo -e "\033[31m KO \033[0m"
                 RET=1
             else
-                echo -ne "\033[32m OK \033[0m"
+                echo -e "\033[32m OK \033[0m"
                 PASSED=$((${PASSED} + 1))
             fi
             TOTAL=$((${TOTAL} + 1))
