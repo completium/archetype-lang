0395ab66c667c615b9e389a860fe2710ed0b5bca
Guillaume Duhamel <guillaume.duhamel@edukera.com>
2020-08-26 17:50:06 +0200

    improve run script

--- extra/script/run.sh
+++ extra/script/run.sh
@@ -1,7 +1,17 @@
 #! /bin/bash
 
+if [ $1 = "--help" ]; then
+echo "$0 contract.arl [argument] [entrypoint]";
+exit 0
+fi
+
 name="$(basename -- $1)"
 id=${name%.*}
+rarg=$2
+rentry=$3
+arg=${rarg:=Unit}
+entrypoint=${rentry:=default}
 
 ./archetype.exe --set-caller-init="tz1Lc2qBKEWCBeDU8npG6zCeCqpmaegRi6Jg" -t michelson $1 > $id.tz
-~/tezos/tezos-client -S -A testnet-tezos.giganode.io -P 443 run script $id.tz on storage $2 and input $3 --entrypoint default
+storage=`./archetype.exe --set-caller-init="tz1Lc2qBKEWCBeDU8npG6zCeCqpmaegRi6Jg" -t michelson-storage $1`
+~/tezos/tezos-client -S -A testnet-tezos.giganode.io -P 443 run script $id.tz on storage "$storage" and input $arg --entrypoint $entrypoint
