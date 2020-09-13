4dfc3f1c976dc9f8d02bbb9a46db7ca277ae45ed
Benoît Rognier <benoit.rognier@edulera.com>
2020-09-13 14:56:56 +0200

    fix fa12 spec

--- src/gen_why3.ml
+++ src/gen_why3.ml
@@ -1678,7 +1678,8 @@ let rec map_mterm m ctx (mt : M.mterm) : loc_term =
         | Ttuple l -> List.length l
         | _ -> assert false
       end in Ttupleaccess (map_mterm m ctx x, (Big_int.int_of_big_int k)+1, card)
-    | Mrecupdate _ -> assert false
+    | Mrecupdate (id, l) ->
+      Trecord (Some (map_mterm m ctx id), List.map (fun (i,t) -> (dl i, map_mterm m ctx t)) l)
     | Masset l ->
       let asset = M.Utils.get_asset_type mt in
       let fns = M.Utils.get_field_list m asset |> wdl in
