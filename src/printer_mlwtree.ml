d8788381dd27c16cd27708d511a59df88d4beec8
Benoît Rognier <benoit.rognier@edulera.com>
2020-09-13 08:21:06 +0200

    maps collection equal

--- src/printer_mlwtree.ml
+++ src/printer_mlwtree.ml
@@ -311,7 +311,7 @@ let rec pp_term outer pos fmt = function
       pp_str (String.capitalize_ascii i)
       (pp_with_paren (pp_term outer pos)) e
   | Teq (Tycoll a, e1, e2) ->
-    Format.fprintf fmt "%a.(==) %a %a"
+    Format.fprintf fmt "%a.eq %a %a"
       pp_str (String.capitalize_ascii a)
       (pp_with_paren (pp_term outer pos)) e1
       (pp_with_paren (pp_term outer pos)) e2
