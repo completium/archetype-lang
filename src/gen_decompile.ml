396ee25e08f33c943f1e37f691bfb77d4e5133fc
Guillaume Duhamel <guillaume.duhamel@edukera.com>
2020-09-26 15:35:53 +0200

    up

--- src/gen_decompile.ml
+++ src/gen_decompile.ml
@@ -85,7 +85,7 @@ let to_ir2 (michelson, _ : T.michelson * 'a) =
   in
 
   let pp_trace ?(with_instr=true) fmt (instr, stack, _sys : T.code * (T.dexpr) list * T.sysofequations) =
-    (* Format.fprintf fmt "@\nsys:@\n%a@\n@\n" Printer_michelson.pp_sysofequations sys; *)
+    Format.fprintf fmt "@\nsys:@\n%a@\n@\n" Printer_michelson.pp_sysofequations sys;
     Format.fprintf fmt "@\nstack:@\n%a" pp_stack stack;
     if with_instr
     then Format.fprintf fmt "@\ninstr: %a@." Printer_michelson.pp_code instr
