497aab7e7fe6112825aefab845bc0ee54d11def0
Guillaume Duhamel <guillaume.duhamel@edukera.com>
2021-10-03 15:44:42 +0200

    inc version

--- src/options.ml
+++ src/options.ml
@@ -16,7 +16,7 @@ let string_to_target_lang = function
   | "markdown"          -> Markdown
   | v                   -> raise (UnknownTarget v)
 
-let version = "1.2.9"
+let version = "1.2.10"
 let url = "https://archetype-lang.org/"
 
 let target = ref (Michelson : target_lang)
