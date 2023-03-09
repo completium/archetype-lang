0ec8c7236634bda59c44c390b803574b1089a799
Guillaume Duhamel <guillaume.duhamel@edukera.com>
2023-03-09 18:45:21 +0100

    up

--- check/bindings/passed/expr_method_asset_nth_aggregate.ts
+++ check/bindings/passed/expr_method_asset_nth_aggregate.ts
@@ -1,15 +1,15 @@
 import * as ex from "@completium/experiment-ts";
 import * as att from "@completium/archetype-ts-types";
-export const o_asset_key_mich_type: att.MichelineType = att.prim_annot_to_mich_type("int", []);
+export const o_asset_key_mich_type: att.MichelineType = att.prim_annot_to_mich_type("nat", []);
 export const my_asset_key_mich_type: att.MichelineType = att.prim_annot_to_mich_type("string", []);
-export const my_asset_value_mich_type: att.MichelineType = att.set_annot_to_mich_type(att.prim_annot_to_mich_type("int", []), []);
-export type o_asset_container = Array<att.Int>;
+export const my_asset_value_mich_type: att.MichelineType = att.set_annot_to_mich_type(att.prim_annot_to_mich_type("nat", []), []);
+export type o_asset_container = Array<att.Nat>;
 export type my_asset_container = Array<[
     string,
-    Array<att.Int>
+    Array<att.Nat>
 ]>;
-export const o_asset_container_mich_type: att.MichelineType = att.set_annot_to_mich_type(att.prim_annot_to_mich_type("int", []), []);
-export const my_asset_container_mich_type: att.MichelineType = att.pair_annot_to_mich_type("map", att.prim_annot_to_mich_type("string", []), att.set_annot_to_mich_type(att.prim_annot_to_mich_type("int", []), []), []);
+export const o_asset_container_mich_type: att.MichelineType = att.set_annot_to_mich_type(att.prim_annot_to_mich_type("nat", []), []);
+export const my_asset_container_mich_type: att.MichelineType = att.pair_annot_to_mich_type("map", att.prim_annot_to_mich_type("string", []), att.set_annot_to_mich_type(att.prim_annot_to_mich_type("nat", []), []), []);
 const exec_arg_to_mich = (): att.Micheline => {
     return att.unit_mich;
 }
@@ -49,27 +49,24 @@ export class Expr_method_asset_nth_aggregate {
     async get_o_asset(): Promise<o_asset_container> {
         if (this.address != undefined) {
             const storage = await ex.get_raw_storage(this.address);
-            return att.mich_to_list((storage as att.Mpair).args[0], x => { return att.Int.from_mich(x); });
+            return att.mich_to_list((storage as att.Mpair).args[0], x => { return att.Nat.from_mich(x); });
         }
         throw new Error("Contract not initialised");
     }
     async get_my_asset(): Promise<my_asset_container> {
         if (this.address != undefined) {
             const storage = await ex.get_raw_storage(this.address);
-            return att.mich_to_map((storage as att.Mpair).args[1], (x, y) => [att.mich_to_string(x), att.mich_to_list(y, x => { return att.Int.from_mich(x); })]);
+            return att.mich_to_map((storage as att.Mpair).args[1], (x, y) => [att.mich_to_string(x), att.mich_to_list(y, x => { return att.Nat.from_mich(x); })]);
         }
         throw new Error("Contract not initialised");
     }
-    async get_res(): Promise<att.Int> {
+    async get_res(): Promise<att.Option<att.Nat>> {
         if (this.address != undefined) {
             const storage = await ex.get_raw_storage(this.address);
-            return att.Int.from_mich((storage as att.Mpair).args[2]);
+            return att.Option.from_mich((storage as att.Mpair).args[2], x => { return att.Nat.from_mich(x); });
         }
         throw new Error("Contract not initialised");
     }
-    errors = {
-        KO: att.string_to_mich("\"ko\""),
-        OUT_OF_BOUND: att.string_to_mich("\"OUT_OF_BOUND\"")
-    };
+    errors = {};
 }
 export const expr_method_asset_nth_aggregate = new Expr_method_asset_nth_aggregate();
--- check/bindings/passed/expr_method_asset_nth_one_field.ts
+++ check/bindings/passed/expr_method_asset_nth_one_field.ts
@@ -46,16 +46,13 @@ export class Expr_method_asset_nth_one_field {
         }
         throw new Error("Contract not initialised");
     }
-    async get_res(): Promise<string> {
+    async get_res(): Promise<att.Option<string>> {
         if (this.address != undefined) {
             const storage = await ex.get_raw_storage(this.address);
-            return att.mich_to_string((storage as att.Mpair).args[1]);
+            return att.Option.from_mich((storage as att.Mpair).args[1], x => { return att.mich_to_string(x); });
         }
         throw new Error("Contract not initialised");
     }
-    errors = {
-        KO: att.string_to_mich("\"ko\""),
-        OUT_OF_BOUND: att.string_to_mich("\"OUT_OF_BOUND\"")
-    };
+    errors = {};
 }
 export const expr_method_asset_nth_one_field = new Expr_method_asset_nth_one_field();
