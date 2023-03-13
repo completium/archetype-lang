2f3eb5a8f73511e45c2a56a4344b8e278bcf8995
Guillaume Duhamel <guillaume.duhamel@edukera.com>
2023-03-13 02:35:52 +0100

    up

--- check/bindings/passed/test_asset.ts
+++ check/bindings/passed/test_asset.ts
@@ -1,52 +1,5 @@
 import * as ex from "@completium/experiment-ts";
 import * as att from "@completium/archetype-ts-types";
-export enum my_enums_types {
-    Init = "Init",
-    InProgress = "InProgress",
-    Completed = "Completed"
-}
-export abstract class my_enums extends att.Enum<my_enums_types> {
-    abstract to_mich(): att.Micheline;
-    equals(v: my_enums): boolean {
-        return att.micheline_equals(this.to_mich(), v.to_mich());
-    }
-}
-export class Init extends my_enums {
-    constructor() {
-        super(my_enums_types.Init);
-    }
-    to_mich() { return new att.Int(0).to_mich(); }
-    toString(): string {
-        return JSON.stringify(this, null, 2);
-    }
-}
-export class InProgress extends my_enums {
-    constructor() {
-        super(my_enums_types.InProgress);
-    }
-    to_mich() { return new att.Int(1).to_mich(); }
-    toString(): string {
-        return JSON.stringify(this, null, 2);
-    }
-}
-export class Completed extends my_enums {
-    constructor() {
-        super(my_enums_types.Completed);
-    }
-    to_mich() { return new att.Int(2).to_mich(); }
-    toString(): string {
-        return JSON.stringify(this, null, 2);
-    }
-}
-export const mich_to_my_enums = (m: any): my_enums => {
-    const v = (new att.Nat((m as att.Mint).int)).to_big_number().toNumber();
-    switch (v) {
-        case 0: return new Init();
-        case 1: return new InProgress();
-        case 2: return new Completed();
-        default: throw new Error("mich_to_asset_type : invalid value " + v);
-    }
-};
 export const my_asset_simple_key_mich_type: att.MichelineType = att.prim_annot_to_mich_type("string", []);
 export const my_asset_key_mich_type: att.MichelineType = att.prim_annot_to_mich_type("int", []);
 export const my_asset_all_key_mich_type: att.MichelineType = att.prim_annot_to_mich_type("address", []);
